fb353c31a33cb820a38241dccc87465aea713faa
Eduardo <peduajo@gmail.com>
2025-06-05 17:31:23 +0200

    fixing CI/CD

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "terraform-state-shared"          # crea este bucket una vez
-    prefix = "composer/${terraform.workspace}" # dev → …/dev/…, prod → …/prod/…
+    bucket = "terraform-state-shared"   # bucket donde guardas el state
+    prefix = "composer"                 # carpeta común
   }
 }
