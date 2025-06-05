197a567312d0baa35430c43fb78ea4d5e765ac01
Eduardo <peduajo@gmail.com>
2025-06-05 12:05:22 +0200

    preparing CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,45 +44,6 @@ resource "google_storage_bucket" "tmp-bucket" {
   }
 }
 
-#resource "google_dataproc_cluster" "spark_cluster" {
-#  name    = "spark-cluster"
-#  region  = var.location          # usa solo la región, p. ej. "europe-west4"
-#  project = var.project_id
-
-#  cluster_config {
-
-#    master_config {
-#      num_instances = 1
-#      machine_type  = "n1-standard-2"
-#      disk_config   { boot_disk_size_gb = 100 }
-#    }
-
-#    worker_config {
-#      num_instances = 2
-#      machine_type  = "n1-standard-4"
-#      disk_config   { boot_disk_size_gb = 100 }
-#    }
-
-#    gce_cluster_config {
-#      service_account = var.service_account_email
-#    }
-
-#    endpoint_config {
-#        enable_http_port_access = "true"
-#    }
-
-#    software_config {
-#      image_version       = "2.2-debian12"
-#      optional_components = ["JUPYTER"]
-
-      # ← AQUÍ usa override_properties, no properties
-#      override_properties = {
-#        "spark:spark.executor.memory" = "2g"
-#      }
-#    }
-#  }
-#}
-
 resource "google_bigquery_dataset" "demo_dataset" {
   dataset_id = var.bq_dataset_name
   location = var.location
